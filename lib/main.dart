56ea90b0b0f6b8920f360dbc9f1340b092ddd6a5
Ghulam Mustafa <ghulammustafa0302666@gmail.com>
2024-05-11 05:03:20 -0700

    cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(FlutterCords());
+  runApp(const MyCards());
 }
-class FlutterCords extends StatelessWidget{
+
+class MyCards extends StatelessWidget{
+  const MyCards({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Cards App",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color: Colors.green,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 33
-          )
-        ),
-        cardTheme: CardTheme(
+        appBarTheme: const AppBarTheme(
           color: Colors.purple,
+          elevation: 24,
+          shadowColor: Colors.black
         ),
-          listTileTheme: ListTileThemeData(
-            titleTextStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.w900,
-              letterSpacing: 3
-            ),
-            subtitleTextStyle: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
-            ),
-            leadingAndTrailingTextStyle: TextStyle(
-                color: Colors.white,
-            ),
-          )
-          // cardColor: Colors.purple,
       ),
-      home: MyCords(),
+      home: const HomePage(),
     );
-  }
 }
 
-class MyCords extends StatelessWidget{
+}
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("This is a card widget"),
-      ),
-      body: Center(
-        child: Container(
-          width: 350,
-          child: Column(
-            children: [
-              Card(
-                // color: Colors.amber,
-                elevation: 24,
-                shadowColor: Colors.green,
-                child: ListTile(
-                  leading: Text("Text"),
-                  title: Text("this is a title"),
-                  subtitle: Text("this is a subtitle"),
-                  trailing: Text("this is a trailing"),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)
-                ),
-              ),
-              Container(height: 40,),
-              Card(
-                // color: Colors.amber,
-                elevation: 24,
-                shadowColor: Colors.green,
-                child: ListTile(
-                  leading: Text("Text"),
-                  title: Text("this is a title"),
-                  subtitle: Text("this is a subtitle"),
-                  trailing: Text("this is a trailing"),
-                ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                ),
-              ),
-              Container(height: 40,),
-              Card(
-                // color: Colors.amber,
-                elevation: 24,
-                shadowColor: Colors.green,
-                child: ListTile(
-                  leading: Text("Text"),
-                  title: Text("this is a title"),
-                  subtitle: Text("this is a subtitle"),
-                  trailing: Text("this is a trailing"),
-                ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                ),
-              ),
-              Container(height: 40,),
-              Card(
-                // color: Colors.amber,
-                elevation: 24,
-                shadowColor: Colors.green,
-                child: ListTile(
-                  leading: Text("Text"),
-                  title: Text("this is a title"),
-                  subtitle: Text("this is a subtitle"),
-                  trailing: Text("this is a trailing"),
-                ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                ),
-              ),
-            ],
-          ),
+        title: const Text(
+          "This is a Card App"
         ),
+      ),
+      body: SizedBox(
+        height: 130,
+          child: Card(
+            elevation: 5, // Set elevation to give shadow effect
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0), // Customize shape
+            ),
+            color: Colors.purple, // Set background color
+            margin: const EdgeInsets.all(10.0), // Set margin
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                            Container(
+                              decoration: BoxDecoration(
+                              ),
+                                width: 70,
+                                height: 70,
+                                  child: Card(
+                                      child: Center(
+                                          child: Text(
+                                            "12",
+                                            style: TextStyle(
+                                                fontSize: 20
+                                            ),
+                                          ),
+                                      ),
+                                  ),
+                              ),
+                          ],
+                      ),
+                      Column(
+                        children: [
+                          Text("Cards"),
+                          Text("Cards"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+
       ),
     );
   }
+
 }
